2d457400fa1ef40b3e23e211abf234b2b578a077
jcooperCUSD <jcooper@chicousd.org>
2021-02-24 09:25:36 -0800

    removed stu.tg = *

--- sql/active-students.sql
+++ sql/active-students.sql
@@ -1,4 +1,4 @@
-SELECT 
+SELECT
  DISTINCT STU.ID as employeeid,
  STU.LN as sn,
  STU.FN as givenname,
@@ -11,7 +11,8 @@ SELECT
 FROM STU
 WHERE
 -- Query both Active and Pre-Enrolled(*) students in the SIS
- ( (STU.del = 0) OR (STU.del IS NULL) ) AND ( STU.tg = ' ' OR (STU.TG = '*') )
+ --( (STU.del = 0) OR (STU.del IS NULL) ) AND ( STU.tg = ' ' OR (STU.TG = '*') )
+ ( (STU.del = 0) OR (STU.del IS NULL) ) AND ( STU.tg = ' ' )
  AND STU.SC IN ( 1,2,3,5,6,7,8,9,10,11,12,13,16,17,18,19,20,21,23,24,25,26,27,28,91 )
  AND STU.BD IS NOT NULL
  AND STU.FN IS NOT NULL

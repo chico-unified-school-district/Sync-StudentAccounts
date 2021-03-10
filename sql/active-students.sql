736f32e2b0cdd1eb0690347f6865ae8ec21212f2
jcooperCUSD <jcooper@chicousd.org>
2021-03-09 16:29:02 -0800

    fixed dn and rootOU compare in usertasks and move user logic

--- sql/active-students.sql
+++ sql/active-students.sql
@@ -7,12 +7,13 @@ SELECT
  STU.U12 as GSuiteStatus,
  STU.GR as grade,
  STU.GR as gecos,
+ STU.TG as enrstatus,
  CONCAT(STU.ID,STU.SC,STU.GR) AS stuRefID
 FROM STU
 WHERE
 -- Query both Active and Pre-Enrolled(*) students in the SIS
- --( (STU.del = 0) OR (STU.del IS NULL) ) AND ( STU.tg = ' ' OR (STU.TG = '*') )
- ( (STU.del = 0) OR (STU.del IS NULL) ) AND ( STU.tg = ' ' )
+ ( (STU.del = 0) OR (STU.del IS NULL) ) AND ( STU.tg = ' ' OR (STU.TG = '*') )
+ --( (STU.del = 0) OR (STU.del IS NULL) ) AND ( STU.tg = ' ' )
  AND STU.SC IN ( 1,2,3,5,6,7,8,9,10,11,12,13,16,17,18,19,20,21,23,24,25,26,27,28,91 )
  AND STU.BD IS NOT NULL
  AND STU.FN IS NOT NULL

46c0ee69be9d389917dbfc01aa46f97056af7267
Justin Cooper <jcooper@chicousd.org>
2024-07-23 15:16:57 -0700

    rolled back ad sessions

--- lib/New-ADSession.ps1
+++ lib/New-ADSession.ps1
@@ -1,14 +1,13 @@
-function New-ADSession ($dcs, $adUser, $cmdlets) {
- for ($i = 0; $i -lt 30; $i++) {
-  foreach ($server in $dcs) {
-   # At least one dc is on at all times unless catastrophe
-   Write-Verbose ('{0},Checking [{1}]' -f $MyInvocation.MyCommand.Name, $server)
-   if (Test-Connection $server -Count 1 -Quiet) { $global:dc = $server; return }
-  }
-  Start-Sleep 10 # If connections to all dcs fail then wait before trying again
- }
- $session = New-PSSession -ComputerName $global:dc -Credential $adUser
- Import-PSSession -Session $session -Module ActiveDirectory -CommandName $cmdlets -AllowClobber | Out-Null
+function New-ADSession ($dc, $cmdlets, $myUser) {
  $msgVars = $MyInvocation.MyCommand.Name, $dc, ($cmdLets -join ',')
  Write-Verbose ('{0},{1}' -f $msgVars)
+ $adSession = New-PSSession -ComputerName $dc -Credential $myUser
+ $sessionParams = @{
+  Session      = $adSession
+  Module       = 'ActiveDirectory'
+  CommandName  = $cmdLets
+  AllowClobber = $true
+  ErrorAction  = 'SilentlyContinue'
+ }
+ Import-PSSession @sessionParams | Out-Null
 }

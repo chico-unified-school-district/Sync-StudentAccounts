7a348de0f65439fb784dd018eb09fe77220be5eb
Justin Cooper <jcooper@chicousd.org>
2025-08-04 10:11:06 -0700

    fix-homedirs updated

--- lib/Fix-HomeDirs.ps1
+++ lib/Fix-HomeDirs.ps1
@@ -5,19 +5,20 @@ param (
  [switch]$WhatIf
 )
 # ==================== Main =====================
+. .\lib\New-HomeDir.ps1
 $lookupTable = Get-Content .\json\lookupTable.json -Raw | ConvertFrom-Json
 
 foreach ($stu in $Students) {
  $sc = [int]$stu.departmentNumber[0]
  $site = $lookupTable.where({ $_.SiteCode -eq $sc })
  $newDirParams = @{
-  FileServer       = $site.FileServer
-  MyShare          = $site.ShareName
-  ServerCredential = $CreateHomeDir
-  Domain           = 'Chico'
-  Samid            = $stu.SamAccountName
-  ModifyAccess     = 'chico\is-all'
-  FullAccess       = 'chico\Domain Admins', 'NT AUTHORITY\SYSTEM', 'chico\Server Admins'
+  FileServer   = $site.FileServer
+  ShareName    = $site.ShareName
+  Credential   = $CreateHomeDir
+  Domain       = 'Chico'
+  Samid        = $stu.SamAccountName
+  ModifyAccess = 'chico\is-all'
+  FullAccess   = 'chico\Server Admins'
  }
  New-HomeDir @newDirParams -WhatIf:$WhatIf
 }

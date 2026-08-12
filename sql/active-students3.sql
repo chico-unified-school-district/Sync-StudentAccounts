f63bb83e789f392cf43022f6b10664a54ee07ece
Justin Cooper <jcooper@chicousd.org>
2026-08-12 15:38:59 -0700

    better gsuite logic

--- sql/active-students3.sql
+++ sql/active-students3.sql
@@ -23,9 +23,10 @@ FROM STU AS s
     ) AS gs
  ON ( s.ID = gs.ID AND s.SC = gs.minimumSitecode )
 WHERE
-(s.FN IS NOT NULL AND s.LN IS NOT NULL AND s.BD IS NOT NULL AND s.GR IS NOT NULL AND s.SC IS NOT NULL)
-AND (s.FN LIKE '%[A-Za-z]%' AND s.LN LIKE '%[A-Za-z]%')
-AND s.SC IN ( VALID_SITE_CODES )
-AND ( (s.DEL = 0) OR (s.DEL IS NULL) ) AND  ( s.TG = ' ' )
+ (s.FN IS NOT NULL AND s.LN IS NOT NULL AND s.BD IS NOT NULL AND s.GR IS NOT NULL AND s.SC IS NOT NULL)
+ AND (s.FN LIKE '%[A-Za-z]%' AND s.LN LIKE '%[A-Za-z]%')
+ AND s.SC IN ( VALID_SITE_CODES )
+ AND ( (s.DEL = 0) OR (s.DEL IS NULL) ) AND  ( s.TG = ' ' )
+ CUSTOM_STUDENT_ID
 -- AND S.ID = 999999 -- testing
 ORDER by s.ID;
